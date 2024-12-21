c10ce950e34cdd9fe956d4b04adc34d09d92230a
aniket44788 <darkpanda44788@gmail.com>
2024-12-21 12:33:08 +0530

    project

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const app = express();
+const mongoose = require("mongoose")
+const env = require("dotenv")
+// app.get("/", (req,res)=>{
+//     res.send("Hey, how are you?")
+// })
+const port = process.env.PORT || 8000   
+env.config()
+
+app.use(express.json())
+const signup = require("./Routes/Signup")
+
+app.use("/signup",signup) 
+
+main().catch((err)=>console.log(err));
+async function main(){
+    await mongoose.connect(process.env.URL);
+    console.log("connected")
+}
+app.listen(8000,()=>{
+    console.log("server is running")
+})
